1f864785ec31a7fac842ac1426c76cbbacfd9f25
Evan Chng Xian Lin <xianlinchng@gmail.com>
2022-10-06 16:22:00 +0800

    Add profile info section

--- src/pages/profilePage/index.tsx
+++ src/pages/profilePage/index.tsx
@@ -1,7 +1,8 @@
 const mockData = {
   name: 'Bob tan',
   gender: 'M',
-  year: 'AY2022/23',
+  batch: 'AY2022/23',
+  year: 2,
   faculty: 'Computing',
   major: 'Computer Science',
   telegram: '@bobbytan',
@@ -11,13 +12,20 @@ const mockData = {
   hobbies: ['fishing', 'coding', 'running'],
   department: 'Software Development',
   role: 'Backend Engineer',
-  projects: ['Atlas HRMS', ['DAO']],
+  projects: ['Atlas HRMS', 'DAO'],
 }
 const ProfilePage = () => {
   return (
-    <div style={{ padding: '48px' }}>
-      <p>Profile Page</p>
-      <ProfilePicture />
+    <div
+      style={{
+        padding: '48px',
+      }}
+    >
+      <h1>Profile Page</h1>
+      <div style={{ display: 'flex', justifyContent: 'center', gap: '24px' }}>
+        <ProfilePicture />
+        <ProfileInfo {...mockData} />
+      </div>
     </div>
   )
 }
@@ -74,6 +82,43 @@ const DeleteImageBtn = () => {
       }}
     >
       Dbtn
+    </div>
+  )
+}
+
+interface ProfileInfoProps {
+  name: string
+  gender: string
+  batch: string
+  year: number
+  faculty: string
+  major: string
+  telegram: string
+  discord: string
+  nus_email: string
+  personal_email: string
+  hobbies: string[]
+  department: string
+  role: string
+  projects: string[]
+}
+const ProfileInfo = (props: ProfileInfoProps) => {
+  return (
+    <div>
+      <p style={{ marginTop: '0px' }}>Name: {props.name}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Batch: {props.batch}</p>
+      <p>Year: {props.year}</p>
+      <p>Faculty: {props.faculty}</p>
+      <p>Major: {props.major}</p>
+      <p>Telegram: {props.telegram}</p>
+      <p>Discord: {props.discord}</p>
+      <p>NUS Email: {props.nus_email}</p>
+      <p>Personal Email: {props.personal_email}</p>
+      <p>Hobbies: {props.hobbies}</p>
+      <p>Department: {props.department}</p>
+      <p>Role: {props.role}</p>
+      <p>Projects: {props.projects}</p>
     </div>
   )
 }
