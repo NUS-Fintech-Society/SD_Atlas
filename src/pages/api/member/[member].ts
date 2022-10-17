a90352a627c684f626bbb28940930770e9501daf
charmainehly <65525578+charmainehly@users.noreply.github.com>
2022-10-17 14:06:32 +0800

    Fix async call for loading single member info

--- src/pages/api/member/[member].ts
+++ src/pages/api/member/[member].ts
@@ -1,7 +1,7 @@
 import type {GetServerSideProps, NextApiRequest, NextApiResponse} from 'next'
 import { prisma } from "../../../server/db/client";
 
-export async function getServerSideProps(context: { params: { studentId: string; }; }) {
+export const getServerSideProps = async (context: { params: { studentId: string; }; }) => {
     const { studentId } = context.params;
     const user = await prisma.user.findUnique({ where: { student_id: studentId } });
     return {
