2ec170a7c10317805d0412545d0c01cbf923eddf
woowenjun99 <woowenjun99@gmail.com>
2022-10-23 19:29:08 +0800

    Add signup page

--- src/pages/admin/create-user.tsx
+++ src/pages/admin/create-user.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 const SignUpPage: NextPage = () => {
   const { data: session, status } = useSession()
-  console.log("THE SESSION IS ", session)
+  const router = useRouter()
+
+  if (!session || session?.level != 2) {
+    return <h1>You do not have the permission to view this page</h1>
+  }
+
   return <h1>Hi There</h1>
 }
 
