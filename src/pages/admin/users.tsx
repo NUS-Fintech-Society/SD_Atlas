207fe0f7ae78e45dec845b33dd855123f0858747
woowenjun99 <woowenjun99@gmail.com>
2022-11-16 17:02:07 +0800

    Add files

---
+++ src/pages/admin/users.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import DashboardPage from "~/components/admin/pages/upload-multiple-users"
+
+const CreateUserPage: NextPage = () => {
+  return <h1>Hi</h1>
+}
+
+export default CreateUserPage
