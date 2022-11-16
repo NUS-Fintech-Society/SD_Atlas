2e17e8b4bb7b6e0bd90d55296a95f13b48e0e8c6
woowenjun99 <woowenjun99@gmail.com>
2022-11-16 17:35:25 +0800

    Clear up the routers

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,16 +1,11 @@
 // src/server/router/index.ts
 import { createRouter } from './context'
 import superjson from 'superjson'
-
-import { exampleRouter } from './example'
-import { protectedExampleRouter } from './protected-example-router'
 import { profileRouter } from './member_profile'
 import dashboardRouter from './dashboard'
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge('example.', exampleRouter)
-  .merge('auth.', protectedExampleRouter)
   .merge('member-profile.', profileRouter)
   .merge('member.', dashboardRouter)
 
