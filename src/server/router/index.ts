bac630d35b1ab6a928f07dc3d743e4958b7ef0df
ziyuanqin <e0563845@u.nus.edu>
2022-12-05 20:39:21 +0800

    add backend to create event and fill table

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -5,12 +5,14 @@ import superjson from 'superjson'
 import { exampleRouter } from './example'
 import { protectedExampleRouter } from './protected-example-router'
 import { profileRouter } from './member_profile'
+import {createEventRouter} from './create-event'
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge('example.', exampleRouter)
   .merge('auth.', protectedExampleRouter)
   .merge('member-profile.', profileRouter)
+  .merge('create-event.', createEventRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter
